b921a9574aeae16c2fabe9be61a60aa3296bf749
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2015-04-24 15:39:18 +0200

    use sprockets to combine existing .js files so only one include is needed

---
+++ sprockets/js/gui.js
@@ -0,0 +1,8 @@
+
+/**
+ * Sprockets include file to combine api scrips
+ * =require external/jquery-jsonview/dist/jquery.jsonview.js
+ * =require gui/Buttons.js
+ * =require gui/Collapsable.js 
+ */
+
